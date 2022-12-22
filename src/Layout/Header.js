71e8b83cd125256e24fd0ab5c1de99372fa2547a
Palanikumar G <palanikumar.cs@gmail.com>
2022-12-22 13:14:46 +0530

    changes Title

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = () => (
   <div>
-    <h1>Hello CodeSandbox</h1>
+    <h1>Hello Palani</h1>
     <h2>Start editing to see some magic happen!</h2>
   </div>
 );
